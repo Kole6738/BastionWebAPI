0f90302c9d9c11897fa73f447428aeffae781c5c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-06 15:38:14 +0000

    routes: implement Redis in twitter router

--- source/routes/v1/twitter.ts
+++ source/routes/v1/twitter.ts
@@ -3,6 +3,8 @@
 import * as express from "express";
 import * as request from "request-promise-native";
 
+import redisClient from "../../redisClient";
+
 /**
  * Interface for User object returned by Twitter API.
  *
@@ -55,87 +57,114 @@ interface User {
 
 module Route {
   export class Twitter {
+    private account: string = 'TheBastionBot';
+    private redisPrefix: string = 'twitter';
+
     public async main(_req: express.Request, res: express.Response, next: express.NextFunction) {
-      try {
-        let url: string = 'https://api.twitter.com/1.1/users/show.json';
-        let options: request.RequestPromiseOptions = {
-          headers: {
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
-          },
-          qs: {
-            screen_name: 'TheBastionBot',
-            include_user_entities: true
-          },
-          json: true
-        };
+      let redisKey: string = `${this.redisPrefix}:${this.account}`;
+      redisClient.get(redisKey, async (err, info) => {
+        try {
+          if (err) next(err);
 
-        let response: TwitterUser = await request(url, options);
+          if (info) {
+            res.json(JSON.parse(info));
+          }
+          else {
+            let url: string = 'https://api.twitter.com/1.1/users/show.json';
+            let options: request.RequestPromiseOptions = {
+              headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
+              },
+              qs: {
+                screen_name: 'TheBastionBot',
+                include_user_entities: true
+              },
+              json: true
+            };
 
-        let info: User = {
-          id: response.id_str,
-          name: response.name,
-          username: response.screen_name,
-          location: response.location,
-          description: response.description,
-          url: response.url,
-          followers_count: response.followers_count,
-          following_count: response.friends_count,
-          statuses_count: response.statuses_count,
-          profile_background_color: response.profile_background_color,
-          profile_image_url: response.profile_image_url_https.replace('_normal', '_400x400'),
-          profile_banner_url: response.profile_banner_url,
-        };
+            let response: TwitterUser = await request(url, options);
 
-        res.json(info);
-      }
-      catch (e) {
-        next(e);
-      }
+            let info: User = {
+              id: response.id_str,
+              name: response.name,
+              username: response.screen_name,
+              location: response.location,
+              description: response.description,
+              url: response.url,
+              followers_count: response.followers_count,
+              following_count: response.friends_count,
+              statuses_count: response.statuses_count,
+              profile_background_color: response.profile_background_color,
+              profile_image_url: response.profile_image_url_https.replace('_normal', '_400x400'),
+              profile_banner_url: response.profile_banner_url,
+            };
+
+            redisClient.setex(redisKey, 86400, JSON.stringify(info));
+
+            res.json(info);
+          }
+        }
+        catch (e) {
+          next(e);
+        }
+      });
     }
 
     public async followers(_req: express.Request, res: express.Response, next: express.NextFunction) {
-      try {
-        let url: string = 'https://api.twitter.com/1.1/followers/list.json';
-        let options: request.RequestPromiseOptions = {
-          headers: {
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
-          },
-          qs: {
-            screen_name: 'TheBastionBot',
-            cursor: -1,
-            count: 200,
-            skip_status: true,
-            include_user_entities: false
-          },
-          json: true
-        };
+      let redisKey: string = `${this.redisPrefix}:${this.account}:followers`;
+      redisClient.get(redisKey, async (err, followers) => {
+        try {
+          if (err) next(err);
 
-        let response: Followers = await request(url, options);
-
-        let followers: User[] = response.users.map((user: TwitterUser) => {
-          return {
-            id: user.id_str,
-            name: user.name,
-            username: user.screen_name,
-            location: user.location,
-            description: user.description,
-            url: user.url,
-            followers_count: user.followers_count,
-            following_count: user.friends_count,
-            statuses_count: user.statuses_count,
-            profile_background_color: user.profile_background_color,
-            profile_image_url: user.profile_image_url_https.replace('_normal', '_400x400'),
-            profile_banner_url: user.profile_banner_url,
+          if (followers) {
+            res.json(JSON.parse(followers));
           }
-        });
+          else {
+            let url: string = 'https://api.twitter.com/1.1/followers/list.json';
+            let options: request.RequestPromiseOptions = {
+              headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
+              },
+              qs: {
+                screen_name: 'TheBastionBot',
+                cursor: -1,
+                count: 200,
+                skip_status: true,
+                include_user_entities: false
+              },
+              json: true
+            };
 
-        res.json(followers);
-      }
-      catch (e) {
-        next(e);
-      }
+            let response: Followers = await request(url, options);
+
+            let followers: User[] = response.users.map((user: TwitterUser) => {
+              return {
+                id: user.id_str,
+                name: user.name,
+                username: user.screen_name,
+                location: user.location,
+                description: user.description,
+                url: user.url,
+                followers_count: user.followers_count,
+                following_count: user.friends_count,
+                statuses_count: user.statuses_count,
+                profile_background_color: user.profile_background_color,
+                profile_image_url: user.profile_image_url_https.replace('_normal', '_400x400'),
+                profile_banner_url: user.profile_banner_url,
+              }
+            });
+
+            redisClient.setex(redisKey, 86400, JSON.stringify(followers));
+
+            res.json(followers);
+          }
+        }
+        catch (e) {
+          next(e);
+        }
+      });
     }
   }
 }
