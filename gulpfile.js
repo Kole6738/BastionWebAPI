80ca06a910de50c57b23db444adb2d9a0c9e07b4
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-20 16:33:44 +0000

    gulp: update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,7 @@
 const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json');
 const del = require('del');
 
 gulp.task('clean', () => {
   // Clean `build` directory
   return del([ 'build/*' ]);
-});
-
-gulp.task('compile', () => {
-  // Compile TypeScript
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('build'));
 });
